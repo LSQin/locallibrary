cf8dedcc44db92d20929e3e8d3ebab07c442fddc
LSQin <1076211669@qq.com>
2021-08-09 20:14:44 +0800

    Registering models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Book, BookInstance, Genre, Language, Author
+
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Author)
