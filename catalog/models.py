6b0314aed5a12d2f46dd59290c6ace548d179419
LSQin <1076211669@qq.com>
2021-08-09 17:47:08 +0800

    Defining the Author Model

--- catalog/models.py
+++ catalog/models.py
@@ -71,3 +71,20 @@ class BookInstance(models.Model):
         """String for representing the Model object"""
         return f'{self.id}({self.book.title})'
 
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_dead = models.DateField('Dead', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+    
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self) -> str:
+        """String for representing the Model object."""
+        return f'{self.first_name},{self.last_name}'
