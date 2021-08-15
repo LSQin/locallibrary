9d3e3895a386d53ae6c3c734acb4787e51c88e67
LSQin <1076211669@qq.com>
2021-08-15 15:12:57 +0800

    Creating home page and books/authors list/detail views
    create url mappers, views, templates to display the pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Book, BookInstance, Author, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Fectch the number of records
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books' : num_books,
+        'num_instances' : num_instances,
+        'num_instances_available' : num_instances_available,
+        'num_authors' : num_authors,
+    }
+
+    # Render the HTML template 'index.html' with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    
