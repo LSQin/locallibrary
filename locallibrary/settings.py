b8aa21de16d4a3e029c868385448c63953c08a44
LSQin <1076211669@qq.com>
2021-08-17 17:53:47 +0800

    Setting up authentication views
    1. add django site authentication urls (for login, logout, password reset)
    2. add templates directory in the template search path
    3. create templates (login.html, logged_out.html, password_reset_form.html, password_reset_email.html, password_reset_done.html, password_reset_confirm.html, password_reset_complete.html)

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +127,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (default redirects to /accounts/profile)
+LOGIN_REDIRECT_URL = '/'
+
+# This logs any emails sent to the console (so you can copy the password reset link from the console).
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
